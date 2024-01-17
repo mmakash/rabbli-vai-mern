24e7645b0ef5190858d36083d5901f2d491ee9c3
akash <musaddekurakash@gmail.com>
2024-01-18 01:04:23 +0600

    productstore request

--- client-site/src/Store/FeatureStore.js
+++ client-site/src/Store/FeatureStore.js
@@ -4,10 +4,11 @@ import axios from "axios";
 const FeatureStore = create((set) => ({
     FeatureList: [],
     FeatureListRequest : async () => {
-        const res = await axios.get("/api/v1/FeatureList");
+        const res = await axios.get("http://localhost:5000/api/v1/FeatureList");
+        console.log(res);
 
         if(res.data.status === "success"){
-            set({FeatureList: res.data.data})
+            await set({FeatureList: res.data.data})
         }
     }
 }))
