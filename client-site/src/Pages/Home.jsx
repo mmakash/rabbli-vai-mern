69d2e337d46c86de5d9ff1b78200296e8ac07f89
ram-elife <ramrachai@elifetransfer.com>
2024-01-18 00:52:46 +0600

    Updated Product store

--- client-site/src/Pages/Home.jsx
+++ client-site/src/Pages/Home.jsx
@@ -1,16 +1,23 @@
 import React, { useEffect } from 'react';
 import Layout from '../Component/layout/Layout';
-import ProductStore from '../Store/ProductStore';
+import useProductStore from '../Store/ProductStore';
 import { useFeatureStore } from '../Store/FeatureStore';
 import MyBrands from '../Component/product/MyBrands';
 import Features from '../Component/product/Features';
 
 const Home = () => {
     const { FeatureList, FeatureListRequest } = useFeatureStore()
-    console.log(FeatureList)
+    const {BrandListRequest,BrandList, CategoryListRequest, CategoryList } = useProductStore()
+    
+    console.log("Feature list ==",FeatureList)
+    console.log("Brand list ==",BrandList)
+    console.log("Category list ==",CategoryList)
+
 
     useEffect(() => {
         FeatureListRequest()
+        BrandListRequest(), 
+        CategoryListRequest()
     }, [])
 
     return (
